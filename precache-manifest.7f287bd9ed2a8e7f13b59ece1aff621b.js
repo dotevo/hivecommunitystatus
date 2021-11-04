a828b23e3acc90d11c01511ade9c46105b7cffc5
Adam Jordanek <dotevo@gmail.com>
2021-11-04 14:56:26 +0100

    1.0.0

---
+++ precache-manifest.7f287bd9ed2a8e7f13b59ece1aff621b.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b2cfae6e8297dc923e2",
+    "url": "/hivecommunitystatus/css/app.2ee2683a.css"
+  },
+  {
+    "revision": "38324a220478f4c83f37f54b698cfa06",
+    "url": "/hivecommunitystatus/img/vue-boilerplates.svg"
+  },
+  {
+    "revision": "71e3160dbbc47fa4b675ad575deb92ee",
+    "url": "/hivecommunitystatus/index.html"
+  },
+  {
+    "revision": "6a064d831c03ca666432",
+    "url": "/hivecommunitystatus/js/about.db5610c0.js"
+  },
+  {
+    "revision": "4b2cfae6e8297dc923e2",
+    "url": "/hivecommunitystatus/js/app.f43af590.js"
+  },
+  {
+    "revision": "57b968ad5e4ce47915b5",
+    "url": "/hivecommunitystatus/js/chunk-vendors~253ae210.e6d282d9.js"
+  },
+  {
+    "revision": "96e54d339aab07fc2f44",
+    "url": "/hivecommunitystatus/js/chunk-vendors~31c671ca.b9f1329a.js"
+  },
+  {
+    "revision": "a74190c86796e05713f2",
+    "url": "/hivecommunitystatus/js/chunk-vendors~36c5d7d2.9347e25c.js"
+  },
+  {
+    "revision": "349735811e7060b6c8c9",
+    "url": "/hivecommunitystatus/js/chunk-vendors~6f27f355.a8155bde.js"
+  },
+  {
+    "revision": "cf1b08a4f4aca8838646",
+    "url": "/hivecommunitystatus/js/chunk-vendors~70aabc29.b6f2bfff.js"
+  },
+  {
+    "revision": "03b6e7146912748d59a6",
+    "url": "/hivecommunitystatus/js/chunk-vendors~8e1f8770.a84dc0ae.js"
+  },
+  {
+    "revision": "c0b584497c9a52a890cd",
+    "url": "/hivecommunitystatus/js/chunk-vendors~93acefaf.d9cb94e1.js"
+  },
+  {
+    "revision": "8ccf52b742dba230269a",
+    "url": "/hivecommunitystatus/js/chunk-vendors~96741198.90af7c2c.js"
+  },
+  {
+    "revision": "f02d88f538398be9b2e2",
+    "url": "/hivecommunitystatus/js/chunk-vendors~f414210c.ebff219f.js"
+  },
+  {
+    "revision": "a963f630596d500bf2e2",
+    "url": "/hivecommunitystatus/js/chunk-vendors~f8ef863f.c202ab03.js"
+  },
+  {
+    "revision": "1c3e2a79e57af7597e70",
+    "url": "/hivecommunitystatus/js/home.ca054565.js"
+  },
+  {
+    "revision": "b653912e8603cce82073",
+    "url": "/hivecommunitystatus/js/runtime~app.4ef82e25.js"
+  },
+  {
+    "revision": "d50014082c0ecae07ee1",
+    "url": "/hivecommunitystatus/js/vue.9cfa1afc.js"
+  },
+  {
+    "revision": "ecfb86c4c4e7661a25373d9f629cd4c6",
+    "url": "/hivecommunitystatus/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/hivecommunitystatus/robots.txt"
+  }
+]);
